c3879b4b1ea943e69ab6d859371d0596c3ef6a9c
Adam Gonzales <adamgonzales1@gmail.com>
2022-07-09 09:44:18 -0700

    feature: move color name and hex value to center of container - update cursor type to copy - make color name and hex value respond when hovered

--- src/components/SchemeDisplay.jsx
+++ src/components/SchemeDisplay.jsx
@@ -1,5 +1,5 @@
-export default function SchemeDisplay ({ schemeColors }) {
-  function handleCopyText (e) {
+export default function SchemeDisplay({ schemeColors }) {
+  function handleCopyText(e) {
     const { innerText } = e.target
     navigator.clipboard.writeText(innerText)
   }
@@ -8,10 +8,18 @@ export default function SchemeDisplay ({ schemeColors }) {
     <div className='color-display'>
       {schemeColors.map((color, i) => {
         return (
-          <div key={i} className='color-display__column' style={{ backgroundColor: `${color.hex.value}` }}>
+          <div
+            key={i}
+            className='color-display__column'
+            style={{ backgroundColor: `${color.hex.value}` }}
+          >
             <ul className='color-display__values'>
-              <li onClick={handleCopyText}>{color.hex.value}</li>
-              <li>{color.name.value}</li>
+              <li tabindex='0' onClick={handleCopyText}>
+                {color.hex.value}
+              </li>
+              <li tabindex='0' onClick={handleCopyText}>
+                {color.name.value}
+              </li>
             </ul>
           </div>
         )
