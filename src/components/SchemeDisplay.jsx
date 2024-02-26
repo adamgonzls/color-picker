8d1c972e24ee8ce6a4d7eb3fbf98020b0f95772d
Adam Gonzales <adamgonzales1@gmail.com>
2024-02-26 15:58:15 -0700

    content: update color info displayed

--- src/components/SchemeDisplay.jsx
+++ src/components/SchemeDisplay.jsx
@@ -1,24 +1,31 @@
 export default function SchemeDisplay({ schemeColors }) {
+  console.log(schemeColors)
   function handleCopyText(e) {
     const { innerText } = e.target
     navigator.clipboard.writeText(innerText)
   }
 
   return (
-    <div className='color-display'>
+    <div className="color-display">
       {schemeColors.map((color, i) => {
         return (
           <div
             key={i}
-            className='color-display__column'
+            className="color-display__column"
             style={{ backgroundColor: `${color.hex.value}` }}
           >
-            <ul className='color-display__values'>
-              <li tabindex='0' onClick={handleCopyText}>
+            <ul className="color-display__values">
+              <li tabIndex="0" onClick={handleCopyText}>
+                {color.name.value}
+              </li>
+              <li tabIndex="0" onClick={handleCopyText}>
                 {color.hex.value}
               </li>
-              <li tabindex='0' onClick={handleCopyText}>
-                {color.name.value}
+              <li tabIndex="0" onClick={handleCopyText}>
+                {color.rgb.value}
+              </li>
+              <li tabIndex="0" onClick={handleCopyText}>
+                {color.hsl.value}
               </li>
             </ul>
           </div>
